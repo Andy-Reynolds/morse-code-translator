d112d121ee521a9803696d13f1e5ca73383c2cd2
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-02-25 12:57:05 +0000

    Removed comments and console logs and tidied up code

--- main.js
+++ main.js
@@ -1,57 +1,12 @@
-// Must import functions
-
+// Import functions
 import { translateLettersToMorse } from "./translator.js";
 
-// Do our dom stuff here
-// const englishToMorseInput = document.getElementById("english-to-morse-input").value;
+// variables
+const englishToMorseInput = document.getElementById("english-to-morse-input").value;
 const englishToMorseButton = document.getElementById("english-to-morse-button");
 const translatedEnglish = document.getElementById("translated-english");
 
-// const lettersToMorseObject = {
-//   "A" :	".-",
-//   "B" :	"-...",	
-//   "C" :	"-.-.",
-//   "D" :	"-..",	
-//   "E" :	".",	
-//   "F" :	"..-.",
-//   "G" :	"--.",	
-//   "H" :	"....",	
-//   "I" :	"..",	
-//   "J" :	".---",	
-//   "K" :	"-.-",	
-//   "L" :	".-..",
-//   "M" :	"--",
-//   "N" :	"-.",	
-//   "O" :	"---",	
-//   "P" :	".--.",	
-//   "Q" :	"--.-",	
-//   "R" :	".-.",
-//   "S" :	"...",	
-//   "T" :	"-",	
-//   "U" :	"..-",	
-//   "V" :	"...-",
-//   "W" :	".--",	
-//   "X" :	"-..-",
-//   "Y" :	"-.--",	
-//   "Z" :	"--.."
-// }
 
 
-// const translateLettersToMorse = () => {
-//   const englishToMorseInput = document.getElementById("english-to-morse-input").value;
-//   // console.log("hello");
-//   // console.log(englishToMorseInput);
-//   // let letter = englishToMorseInput
-//   // console.log(letter);
-//   let upperCaseLetter = englishToMorseInput.toUpperCase();
-//   // console.log(upperCaseLetter);
-//   // console.log(lettersToMorseObject[upperCaseLetter]);
-//   let finalTranslation = lettersToMorseObject[upperCaseLetter];
-//   // return lettersToMorseObject[upperCaseLetter];
-//   translatedEnglish.innerHTML = finalTranslation;
-// };
-
-
-
-// on button click, a function is run that translates text and changes the innerhtml of translated english div
+// on button click, runs translateLettersToMorse function
 englishToMorseButton.addEventListener("click", translateLettersToMorse);
--- main.test.js
+++
@@ -1,3 +0,0 @@
-import {expect,it,describe} from "@jest/globals";
-
-// Must import functions from main.js
