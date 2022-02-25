e026f2879acd54af0825aec4eae165b2bd2d6b07
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-02-25 11:02:38 +0000

    started on a for loop with lots of if statements. This works but is very inefficient

--- translator.js
+++ translator.js
@@ -2,43 +2,59 @@
 
 // english to morse translator function
 // export const translateEnglishToMorse = (englishWord) => {
+  
+  // };
+  
 
-// };
+const A =	".-";	
+const B =	"-...";	
+const C =	"-.-.";
+const D =	"-..";	
+const E =	".";	
+const F =	"..-.";
+const G =	"--.";	
+const H =	"....";	
+const I =	"..";	
+const J =	".---";	
+const K =	"-.-";	
+const L =	".-..";
+const M =	"--";	
+const N =	"-.";	
+const O =	"---";	
+const P =	".--.";	
+const Q =	"--.-";	
+const R =	".-.";
+const S =	"...";	
+const T =	"-";	
+const U =	"..-";	
+const V =	"...-";	
+const W =	".--";	
+const X =	"-..-";
+const Y =	"-.--";	
+const Z =	"--..";
+
+
+
+let englishWord = "abc";
+
+for (let i = 0; i < englishWord.length; i++) {
+  let wordCharacters = englishWord.charAt(i);
+  console.log(wordCharacters);
+  if (wordCharacters == "a") {
+    console.log(A);
+  };
+  if (wordCharacters == "b") {
+    console.log(B);
+  };
+  if (wordCharacters == "c") {
+    console.log(C);
+  };
+}
 
 // each combo of morse can be a string variable. e.g.
 // const a = ".-";
 
+// first need a function that returns each character of a string
+// for each character, the
 
-let word = "hello";
 
-for (let i = 0; i < word.length; i++) {
-  console.log(word.charAt(i));
-}
-
-// A	.-	
-// B	-...	
-// C	-.-.	
-// D	-..	
-// E	.	
-// F	..-.
-// G	--.	
-// H	....	
-// I	..	
-// J	.---	
-// K	-.-	
-// L	.-..
-// M	--	
-// N	-.	
-// O	---	
-// P	.--.	
-// Q	--.-	
-// R	.-.
-// S	...	
-// T	-	
-// U	..-	
-// V	...-	
-// W	.--	
-// X	-..-
-// Y	-.--	
-// Z	--..
-// space /
